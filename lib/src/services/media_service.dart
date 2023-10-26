73be8464ef7ce99bc17f377a5e5811bec618c6b5
TyagiLikeMinds <anurag.tyagi@likeminds.community>
2023-10-26 21:08:06 +0530

    added defensive check for media upload name

--- lib/src/services/media_service.dart
+++ lib/src/services/media_service.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:likeminds_feed_ss_fl/src/utils/credentials/credentials.dart';
-import 'package:path/path.dart';
+import 'package:path/path.dart' as path;
 
 import 'package:simple_s3/simple_s3.dart';
 
@@ -18,16 +18,25 @@ class MediaService {
 
   Future<String?> uploadFile(File file, String userUniqueId) async {
     try {
-      String fileName = basenameWithoutExtension(file.path);
+      String extension = path.extension(file.path);
+      String fileName = path.basenameWithoutExtension(file.path);
+      fileName = fileName.replaceAll(RegExp('[^A-Za-z0-9]'), '');
       String currTimeInMilli = DateTime.now().millisecondsSinceEpoch.toString();
+      fileName = '$fileName-$currTimeInMilli$extension';
+
+      String dir = path.dirname(file.path);
+      String newPath = path.join(dir, fileName);
+
+      File renamedFile = file.copySync(newPath);
 
       String result = await _s3Client.uploadFile(
-        file,
+        renamedFile,
         _bucketName,
         _poolId,
         _region,
-        s3FolderPath: "files/post/$userUniqueId/$fileName-$currTimeInMilli",
+        s3FolderPath: "files/post/$userUniqueId",
       );
+
       return result;
     } on SimpleS3Errors catch (e) {
       debugPrint(e.name);
