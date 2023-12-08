893ee6ba45dc0a956b385296a15d4935090e1b61
Anuj Kumar <144224503+AnujLM@users.noreply.github.com>
2023-12-08 19:10:13 +0530

    feat(deeplink): added multiple deeplink handler

--- lib/src/utils/utils.dart
+++ lib/src/utils/utils.dart
@@ -3,3 +3,5 @@ export 'analytics/analytics.dart';
 export 'notifications/notification_handler.dart';
 
 export 'local_preference/user_local_preference.dart';
+
+export 'deep_link/deep_link_handler.dart';
