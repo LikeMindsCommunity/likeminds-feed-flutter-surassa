1340fdbf6ba3b5e245ed66617c0841dcd8dd8695
TyagiLikeMinds <anurag.tyagi@likeminds.community>
2024-02-02 19:10:39 +0530

    remove unused code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -33,7 +33,7 @@ void main() async {
 
   await LMFeedCore.instance.initialize(
     // lmFeedClient: lmFeedClient,
-    apiKey: "6b51af13-ce28-444b-a571-53a3fb125444",
+
     domain: "suraasalearn://www.suraasa.com/community/",
     config: LMFeedConfig(
       composeConfig: const LMFeedComposeScreenConfig(
