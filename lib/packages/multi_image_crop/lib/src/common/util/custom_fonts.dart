73aeb93d987d3d20756dc7b1fbcce2d32f639ede
TyagiLikeMinds <anurag.tyagi@likeminds.community>
2024-02-02 22:56:53 +0530

    remove: tagging package

--- lib/packages/multi_image_crop/lib/src/common/util/custom_fonts.dart
+++
@@ -1,8 +0,0 @@
-class CustomFonts {
-  static const font1 = 'Bandar';
-  static const font2 = 'Typewriter';
-  static const font3 = 'Highup';
-  static const font4 = 'Shogeking';
-  static const font5 = 'Darlington';
-  static const font6 = 'Default';
-}
